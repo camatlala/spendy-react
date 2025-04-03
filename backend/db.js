7d0e3faf1842624b3f7a41e29b44185a796a701a
camatlala <131349664+camatlala@users.noreply.github.com>
2025-04-03 10:03:59 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -4,27 +4,27 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI || "mongodb+srv://antoniomatlala02:lnm47kVFVCJzLhR0@spendy.vbodvqm.mongodb.net/?retryWrites=true&w=majority&appName=spendy";
 
 const client = new MongoClient(uri, {
-    serverApi: {
+  serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-    }
+  }
 });
 
 let db;
 
 async function connectToMongo() {
-    try {
+  try {
     await client.connect();
     db = client.db("spendy"); // Use your actual database name here
     console.log("✅ Connected to MongoDB Atlas!");
-    } catch (error) {
+  } catch (error) {
     console.error("❌ MongoDB connection failed:", error);
-    }
+  }
 }
 
 function getDB() {
-    return db;
+  return db;
 }
 
 module.exports = { connectToMongo, getDB };
